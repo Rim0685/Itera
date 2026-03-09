91b8d74435499c36af4d0f3fc4df628a2389bd41
Rim <rikas0685@oslomet.no>
2026-03-09 11:59:05 +0100

    save

--- tests/saucedemo.test.ts
+++ tests/saucedemo.test.ts
@@ -2,10 +2,12 @@ import { test, expect } from '@playwright/test';
 test('Gå inn på demo', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
 
-    await expect (page.tohaveURL('https://www.saucedemo.com/'));
+    await expect (page).tohaveURL('https://www.saucedemo.com/');
 
     await page.getByPlaceholder('Username').fill('standard_user');
 
-    await page.getByTestId('password').fill('passord');
+    await page.getByTestId('password').fill('password');
 
+    await page.locator('#login-button').click();
+    await expect (page).tohaveURL('https://www.saucedemo.com/inventory.html');
 });
